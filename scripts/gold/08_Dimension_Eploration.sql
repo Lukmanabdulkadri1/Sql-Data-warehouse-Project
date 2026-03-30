ca946e13e055287e3bc44e40a53bdc54cf9ffc2f
Lukman Abdulkadri <Lukmanabdulkadri1@gmail.com>
2026-03-30 07:56:03 -0700

    Create 08_Dimension_Exploration.sql for data exploration
    Add SQL script to identify unique values in dimensions.

---
+++ scripts/gold/08_Dimension_Eploration.sql
@@ -0,0 +1,11 @@
+--Identify the Unique Values(Or categories) in each dimension
+
+SELECT DISTINCT
+	country
+FROM gold.dim_customers
+
+-- Explore All Categories 'The Major Divisions'
+SELECT DISTINCT
+	category,subcategory,product_name
+FROM gold.dim_products
+
